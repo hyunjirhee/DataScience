eaadaae0b06488261aa5ad54dd220698e6f94045
hyunjirhee <hhcrhh@gmail.com>
2021-04-01 16:33:04 +0900

    test

---
+++ fileAnalysis.java
@@ -0,0 +1,25 @@
+package rhee;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class fileAnalysis {
+
+    public static String DIR = ""
+
+    public static void descriptiveAnalysis(String[] args) throws IOException {
+        BufferedReader BR = new BufferedReader(new FileReader(DIR + filename));
+
+        System.out.println("*****************NUMERICAL ANALYSIS*****************");
+        String line;
+        String line2;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
